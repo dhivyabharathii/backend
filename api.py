20c6d6ad36e33739cf66ef142a73a24f13abb35e
dhivyabharathii <126810207+dhivyabharathii@users.noreply.github.com>
2023-04-21 12:34:11 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -13,8 +13,8 @@ Database_url=os.getenv('Database_url')
 secret=os.getenv('secret')
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost:5432/flaskk'
+app.config['SECRET_KEY']=secret
+app.config['SQLALCHEMY_DATABASE_URI']=Database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['JWT_SECRET_KEY'] = 'super-secret'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
